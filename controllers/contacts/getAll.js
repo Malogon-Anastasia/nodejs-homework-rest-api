09171fe75ba44d2b8621d33bfb22dd79044cedb3
Malogon Anastasua <malogonanastasia@gmail.com>
2022-06-12 17:00:59 +0300

    pagination create

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,10 @@ const {Contact} = require("../../models")
 
 const getAll = async (req, res) => {
     const { _id } = req.user;
-    const contacts = await Contact.find({owner: _id}).populate("owner", "_id name email subscription");
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+
+    const contacts = await Contact.find({owner: _id}, "", {skip, limit: Number(limit)}).populate("owner", "_id name email subscription");
     res.json({
         status: "success",
         code: 200,
