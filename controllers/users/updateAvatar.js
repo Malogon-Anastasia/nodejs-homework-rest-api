ef78a7fdb53cd1c02cef9894f4ccbb4cca0cfcc0
Malogon Anastasua <malogonanastasia@gmail.com>
2022-06-18 16:47:38 +0300

    create controller updateAvatar

---
+++ controllers/users/updateAvatar.js
@@ -0,0 +1,26 @@
+const {User} = require("../../models");
+const path = require("path");
+const fs = require("fs/promises");
+
+
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async(req, res) => {
+    const {path: tempUpload, originalname} = req.file;
+try {
+    const resultUpload = path.join(avatarsDir, originalname);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarUrl = path.join("public", "avatars", originalname);
+    await User.findByIdAndUpdate(req.user._id, {avatarUrl});
+    res.json({avatarUrl});
+} catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
+}
+
+
+};
+
+module.exports = updateAvatar;
+
+ 
