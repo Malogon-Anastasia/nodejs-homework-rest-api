dc872c7b5f982be8e688a71b71244a2e24b9fcbf
Malogon Anastasua <malogonanastasia@gmail.com>
2022-06-18 17:12:52 +0300

    finish with avatars

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -7,17 +7,18 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updateAvatar = async(req, res) => {
     const {path: tempUpload, originalname} = req.file;
-try {
-    const resultUpload = path.join(avatarsDir, originalname);
-    await fs.rename(tempUpload, resultUpload);
-    const avatarUrl = path.join("public", "avatars", originalname);
-    await User.findByIdAndUpdate(req.user._id, {avatarUrl});
-    res.json({avatarUrl});
-} catch (error) {
-    await fs.unlink(tempUpload);
-    throw error;
+    const { _id: id } = req.user;
+    const avatarName = `${id}_${originalname}`;
+    try {
+        const resultUpload = path.join(avatarsDir, avatarName);
+        await fs.rename(tempUpload, resultUpload);
+        const avatarUrl = path.join("public", "avatars", avatarName);
+        await User.findByIdAndUpdate(req.user._id, {avatarUrl});
+        res.json({avatarUrl});
+        } catch (error) {
+            await fs.unlink(tempUpload);
+            throw error;
 }
-
 
 };
 
