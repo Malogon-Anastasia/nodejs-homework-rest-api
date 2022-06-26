45acd566adf5b2c0b671c0b6c7392c7694b64a9c
Malogon Anastasua <malogonanastasia@gmail.com>
2022-06-26 16:43:20 +0300

    email varification

--- models/user.js
+++ models/user.js
@@ -30,7 +30,15 @@ const userSchema = Schema(
         avatarURL: {
           type: String,
           require: true
-      }
+      },
+      verify: {
+        type: Boolean,
+        default: false,
+      },
+      verificationToken: {
+        type: String,
+        required: [true, 'Verify token is required'],
+      },    
     },
       { versionKey: false, timestamps: true },
 );
--- routes/api/users.js
+++ routes/api/users.js
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.get("/current", auth, ctrlWrapper(ctrl.getCarrent));
 router.patch("/avatars", auth, upload.single("avatar"), ctrlWrapper(ctrl.updateAvatar));
+router.get("/verify/:verificationToken", ctrlWrapper(ctrl.verifyEmail))
 
 module.exports = router;
